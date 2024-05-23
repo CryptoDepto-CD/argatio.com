1e7ee35a3650333d72a1162fb3b9bc72988f7082
Jesus Rojas <rojasalarcone@gmail.com>
2024-05-23 09:08:08 -0500

    build: adding storybook dependencies

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["../public"],
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
---
+++ .storybook/preview.js
@@ -0,0 +1,24 @@
+import "../src/app/globals.css";
+import { themes } from "@storybook/theming";
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      values: [
+        {argatio: '#0B0B0B'}
+      ]
+    }
+  },
+};
+
+export default preview;
