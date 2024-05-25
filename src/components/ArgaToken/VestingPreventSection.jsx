3539b4cf257606dc32677b885a4b52e0c57a2387
Jesus Rojas <rojasalarcone@gmail.com>
2024-05-25 17:38:40 -0500

    feat(argatoken): Adding the Prevent section

---
+++ src/components/ArgaToken/VestingPreventSection.jsx
@@ -0,0 +1,30 @@
+import Phases from "@/components/ui/ArgaToken/Phases";
+import BuyArgaToken from "../ui/ArgaToken/BuyArgaToken/BuyArgaToken";
+
+export default function VestingPreventSection() {
+  return (
+    <section className="px-10 py-10">
+      <div className="flex flex-row items-center justify-center">
+        <div
+          className={`font-nats aspect-square rounded-full border-[clamp(15px,2.5vw,25px)] flex items-center text-center w-fit scale-[0.6]`}
+        >
+          <p className="text-clamp-ultrabig uppercase leading-none px-[clamp(15px,3vw,45px)]">
+            Arga
+            <br />
+            Token
+          </p>
+        </div>
+        <div className="uppercase">
+          <p className="leading-none text-[clamp(1.5rem,3.6vw,4rem)] font-nats">Preventa</p>
+          <p className="leading-none text-clamp-ultrabig font-nats">VESTING</p>
+        </div>
+      </div>
+      <div className="">
+        <Phases />
+      </div>
+      <div className="mx-auto my-16 max-w-80">
+        <BuyArgaToken />
+      </div>
+    </section>
+  );
+}
