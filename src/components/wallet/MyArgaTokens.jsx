84acd7582f65ad7c2882bffe654e0ad8a3117afd
Jesus Rojas <rojasalarcone@gmail.com>
2024-05-25 17:56:54 -0500

    feat(wallet): Adding the My Arga Tokens component

---
+++ src/components/wallet/MyArgaTokens.jsx
@@ -0,0 +1,36 @@
+
+
+export default function MyArgaTokens() {
+  return (
+    <div className="py-4 my-auto">
+      <h3 className="mx-auto mb-5 leading-none text-center uppercase text-clamp-title font-nats">
+        Mis Arga Tokens
+      </h3>
+      <p className="py-1 mb-5 font-bold text-center border-b-2 border-black border-solid text-monserrat text-clamp-text">Fase <span>01</span></p>
+      <div className="flex flex-wrap items-center gap-8 justify-evenly font-montserrat text-clamp-text">
+        <div className="*:mb-2">
+          <p className="font-medium uppercase">Adquirido</p>
+          <p className="flex gap-5">
+            <span className="min-w-[7ch]">14.29</span>
+            <span>Arga tokens</span>
+          </p>
+          <p className="flex gap-5">
+            <span className="min-w-[7ch]">1.0</span>
+            <span>$US</span>
+          </p>
+        </div>
+        <div className="*:mb-2">
+          <p className="font-medium uppercase">Liberados</p>
+          <p className="flex gap-5">
+            <span className="min-w-[7ch]">00.00</span>
+            <span>Arga tokens</span>
+          </p>
+          <p className="flex gap-5">
+            <span className="min-w-[7ch]">0.0</span>
+            <span>$US</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
