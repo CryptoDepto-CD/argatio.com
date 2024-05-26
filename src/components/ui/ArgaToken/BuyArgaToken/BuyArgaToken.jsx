6101b3fb7c534ae54895512f00b6b4f66c609d40
Damián <damian.panes@inacapmail.cl>
2024-05-26 16:17:18 -0400

    Fix: Actualizando el botón y quitando el dialog para que envie directamente a autenticarse a el usuario.

--- src/components/ui/ArgaToken/BuyArgaToken/BuyArgaToken.jsx
+++ src/components/ui/ArgaToken/BuyArgaToken/BuyArgaToken.jsx
@@ -1,17 +1,23 @@
 "use client";
 
-import LogInDialog from '@/components/ArgaToken/Dialogs/LogInDialog'
-import Button from '../../Button/Button'
-import { useState } from 'react'
+import LogInDialog from "@/components/ArgaToken/Dialogs/LogInDialog";
+import Button from "../../Button/Button";
+import { useState } from "react";
+
+import ButtonBlockchain from "../../ButtonBlockchain/ButtonBlockchain";
 
 export default function BuyArgaToken() {
-
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className='w-full'>
-      <Button type="button" onclick={() => setIsOpen(true)}>Comprar Arga Token</Button>
-      <LogInDialog open={isOpen} handleClick={() => setIsOpen(false)} />
+    <div className="w-full">
+      <ButtonBlockchain type="button" btnTitle="Comprar Arga Token">
+        <Button type="link" href="/wallet">
+          Comprar Arga Token
+        </Button>
+      </ButtonBlockchain>
+      {/* <Button type="button" onclick={() => setIsOpen(true)}>Comprar Arga Token</Button>
+      <LogInDialog open={isOpen} handleClick={() => setIsOpen(false)} /> */}
     </div>
-  )
+  );
 }
