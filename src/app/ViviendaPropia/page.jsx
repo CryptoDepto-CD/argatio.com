edc09aeabcdaab0ab000c375eb7d9d297e7ecda6
Damián <damian.panes@inacapmail.cl>
2024-05-26 12:36:34 -0400

    fix: arreglando rutas

--- src/app/ViviendaPropia/page.jsx
+++ src/app/ViviendaPropia/page.jsx
@@ -1,13 +1,13 @@
-import BgForms from "@/../public/ViviendaPropia/forms_bg.svg"
+import BgForms from "@/../public/ViviendaPropia/forms_bg.svg";
 
 import Image from "next/image";
 
-import HeroSection from "@/components/ViviendaPropia/HeroSection";
-import ApplyAsBuyerSection from "@/components/viviendaPropia/ApplyAsBuyerSection";
-import ApplyPropertySection from "@/components/viviendaPropia/ApplyPropertySection";
-import HowItWorksSection from "@/components/viviendaPropia/HowItWorksSection";
-import WhyOwnHome from "@/components/viviendaPropia/WhyOwnHome";
-import NeedHelpSection from "@/components/viviendaPropia/NeedHelpSection";
+import HeroSection from "../../components/viviendaPropia/HeroSection";
+import ApplyAsBuyerSection from "../../components/viviendaPropia/ApplyAsBuyerSection";
+import ApplyPropertySection from "../../components/viviendaPropia/ApplyPropertySection";
+import HowItWorksSection from "../../components/viviendaPropia/HowItWorksSection";
+import WhyOwnHome from "../../components/viviendaPropia/WhyOwnHome";
+import NeedHelpSection from "../../components/viviendaPropia/NeedHelpSection";
 
 export default function ViviendaPropia() {
   return (
@@ -15,13 +15,20 @@ export default function ViviendaPropia() {
       <HeroSection />
       <HowItWorksSection />
       <WhyOwnHome />
-      <div id="homeSection" className="relative flex flex-col items-center p-10 justify-evenly lg:flex-row gap-14">
+      <div
+        id="homeSection"
+        className="relative flex flex-col items-center p-10 justify-evenly lg:flex-row gap-14"
+      >
         <ApplyPropertySection />
         <div className="hidden w-2 h-[clamp(600px,90vh,1000px)] bg-white lg:block"></div>
         <ApplyAsBuyerSection />
-        <Image src={BgForms} alt="background" className="absolute px-5 -z-10 max-w-[1000px]" />
+        <Image
+          src={BgForms}
+          alt="background"
+          className="absolute px-5 -z-10 max-w-[1000px]"
+        />
       </div>
       <NeedHelpSection />
     </main>
-  )
+  );
 }
