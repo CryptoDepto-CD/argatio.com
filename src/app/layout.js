ef37a786fa8b4b888f291bf79112eddd2990e5b2
Damián <damian.panes@inacapmail.cl>
2024-05-27 19:01:36 -0400

    chore: Update analytics.js for Google Analytics and Facebook Pixel integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -47,7 +47,28 @@ export default function RootLayout({ children }) {
             walletConnect(),
           ]}
         >
-          <Script src="/analytics.js" strategy="afterInteractive"></Script>
+
+          {/* Analiticas */}
+
+          <Script strategy="afterInteractive" async>
+            {`
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+             gtag('config', 'G-ZEZMFFPQPF');
+
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1001366404856784');
+            fbq('track', 'PageView');
+            `}
+          </Script>
           <NavBar />
           {children}
           <Footer />
