ad45f4ed26635a31e69cd44e290627954cee56d0
Cap <capde22@gmail.com>
2024-05-27 11:37:23 -0300

    feat:vesting purchase

--- src/app/wallet/page.jsx
+++ src/app/wallet/page.jsx
@@ -4,13 +4,52 @@ import InvestSection from "@/components/wallet/InvestSection";
 import VestingPreventSection from "@/components/wallet/VestingPreventSection";
 import WalletSection from "@/components/wallet/WalletSection";
 import ButtonBlockchain from "@/components/ui/ButtonBlockchain/ButtonBlockchain";
+import { ConnectWallet } from "@thirdweb-dev/react";
+import { useAddress, useDisconnect } from "@thirdweb-dev/react";
 
 export default function Wallet() {
+
+  const address = useAddress()
+  const disconnect = useDisconnect();
+
   return (
     <main>
-      <VestingPreventSection />
-      <InvestSection />
-      <WalletSection />
+
+      {!address ? (
+        <div className="w-full min-h-screen flex items-center justify-center">
+          <div>
+            <ConnectWallet
+              btnTitle={'Iniciar sesión'}
+              modalTitle="Welcome"
+              hideTestnetFaucet={true}
+              hideSwitchToPersonalWallet={true}
+              showThirdwebBranding={false}
+              className=" !rounded-full !px-10 !py-1 !font-semibold !containertext-center !text-black !transition-colors !bg-white !hover:bg-black !outline !grow !font-montserrat !hover:text-white"
+            />
+          </div>
+
+        </div>
+
+      ) : (
+        <>
+          <div className="w-full flex justify-end mr-16">
+            <div>
+              <button
+                className=" mx-12 rounded-full px-10 py-1 font-semibold containertext-center text-black transition-colors bg-white hover:bg-black outline grow font-montserrat hover:text-white"
+                onClick={disconnect}
+              >
+                Desconectar
+              </button>
+            </div>
+          </div>
+
+          <VestingPreventSection />
+          <InvestSection />
+          <WalletSection />
+        </>
+      )}
+
+
     </main>
   );
 }
--- src/components/web3Buttons/VestingButton.jsx
+++ src/components/web3Buttons/VestingButton.jsx
@@ -1,35 +1,30 @@
 import { useContractWrite, useContract, Web3Button } from "@thirdweb-dev/react";
 import React from "react";
-import ERC20ABI from '../../utils/ERC20.json'
 import GlobalVestingABI from '../../utils/OldVesting.json'
 import VestingABI from '../../utils/Vesting.json'
-import { ethers } from "ethers";
-import { addressUSDT, addressUSDC, addressBUSD } from "@/utils/constants";
+import { addressVestingARGA } from "@/utils/constants";
 import { GetWeiAmount } from "@/hooks/useBlockchain";
 
-export function ERC20Button({
+export function VestingButton({
     type,
-    tokenAddress,
-    tokenName,
-    amount,
-    to,
-    spender,
+    stableAddress,
+    stableName,
+    stableAmount,
     onSuccessFunction,
     onErrorFunction,
-    stableToken,
     disabled
 }) {
 
-    var amountInWei = GetWeiAmount(tokenAddress, amount)
+    var amountInWei = GetWeiAmount(stableAddress, stableAmount)
 
-    const { contract } = useContract(tokenAddress)
+    const { contract } = useContract(addressVestingARGA)
     const { mutateAsync, isLoading, error } = useContractWrite(contract, type);
 
     const getAction = () => {
 
         switch (type) {
-            case "approve":
-                return () => mutateAsync({ args: [spender, amountInWei] });
+            case "invest":
+                return () => mutateAsync({ args: [stableAddress, amountInWei] });
 
             default:
                 throw new Error(`Unsupported transaction type: ${type}`);
@@ -38,14 +33,14 @@ export function ERC20Button({
 
     return (
         <Web3Button
-            contractAddress={tokenAddress}
+            contractAddress={addressVestingARGA}
             action={getAction()}
             onSuccess={() => onSuccessFunction()}
             onError={(error) => onErrorFunction(error)}
             isDisabled={disabled}
-            contractAbi={ERC20ABI}
+            contractAbi={VestingABI}
         >
-            {type === "approve" && `Aprobar ${amount} ${tokenName}`}
+            {type === "invest" && `Comprar con ${stableAmount} ${stableName}`}
         </Web3Button>
     );
 }
