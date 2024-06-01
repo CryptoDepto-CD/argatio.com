0e818afc59c8f212a935b6bf48ceeb948ea25b1b
Jesus Rojas <rojasalarcone@gmail.com>
2024-05-31 20:02:34 -0500

    feat: adding the About Us page

---
+++ src/app/AboutUs/page.jsx
@@ -0,0 +1,19 @@
+import DiscoverMoreSection from "@/components/aboutUs/DiscoverMoreSection";
+import HeroSection from "@/components/aboutUs/HeroSection";
+import MissionSection from "@/components/aboutUs/MissionSection";
+import OurTeamSection from "@/components/aboutUs/OurTeamSection";
+import SocialMediaSection from "@/components/aboutUs/SocialMediaSection";
+import WhatWeDoSection from "@/components/aboutUs/WhatWeDoSection";
+
+export default function AboutUs() {
+  return (
+    <main className="">
+      <HeroSection />
+      <MissionSection />
+      <WhatWeDoSection />
+      <OurTeamSection />
+      <DiscoverMoreSection />
+      <SocialMediaSection />
+    </main>
+  )
+}
