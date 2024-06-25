d8f0fb5d6ca8c94d42e5283c1913f56f992a56d7
Josue <josuepalvh@gmail.com>
2024-06-25 10:46:47 -0500

    feat(airdrop): :sparkles: I created the new rute airdrop, and download the svg's for the heroSection

---
+++ src/app/[locale]/airdrop/page.jsx
@@ -0,0 +1,10 @@
+import HeroSection from '@/components/airdrop/HeroSection'
+import React from 'react'
+
+export default function Airdrop() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  )
+}
---
+++ src/components/airdrop/HeroSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+    <section>
+        <h2 className=' text-4xl font-semibold text-center p-5'>Airdrop</h2>
+    </section>
+  )
+}
