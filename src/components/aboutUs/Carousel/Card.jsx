cd1e035b66ac8bdae2838955487aac4ca5234a05
Josue <josuepalvh@gmail.com>
2024-07-02 19:28:40 -0500

    fix(about): :bug: solved the problem of grid and the height of the cards

--- src/components/aboutUs/Carousel/Card.jsx
+++ src/components/aboutUs/Carousel/Card.jsx
@@ -5,15 +5,19 @@ import Link from "next/link";
 
 function Card({ linkedin, image, name, role, birthday }) {
   return (
-    <div className="relative p-5 flex-col gap-3 flex shadow-[0px_4px_5px_rgba(255,255,255,0.16)_inset,0px_1px_4px_rgba(11,55,0,0.27)] backdrop-blur[7.5px] rounded-[19px] [background:linear-gradient(120.89deg,_rgba(255,_255,_255,_0.1),_rgba(255,_255,_255,_0.02))] text-center">
-      <Link href={linkedin} className="w-10 h-10 p-2 -mb-8 bg-blue-700 rounded-lg aspect-square">
-        <Linkedin size={24} />
-      </Link>
-      <div className="aspect-square border-[5px] border-solid border-white rounded-full mb-5">
-        <Image src={image} alt={name} className="w-full aspect-square" width="100" height="100" />
+    <div className="relative p-5 h-full flex-col justify-between gap-3 flex shadow-[0px_4px_5px_rgba(255,255,255,0.16)_inset,0px_1px_4px_rgba(11,55,0,0.27)] backdrop-blur[7.5px] rounded-[19px] [background:linear-gradient(120.89deg,_rgba(255,_255,_255,_0.1),_rgba(255,_255,_255,_0.02))] text-center">
+      <div className=" flex flex-col">
+        <Link href={linkedin} className="w-10 h-10 p-2 -mb-8 bg-blue-700 rounded-lg aspect-square">
+          <Linkedin size={24} />
+        </Link>
+        <div className="aspect-square border-[5px] border-solid border-white rounded-full mb-5">
+          <Image src={image} alt={name} className="w-full aspect-square" width="100" height="100" />
+        </div>
       </div>
-      <p className="leading-none uppercase font-nats text-clamp-subtitle">{name}</p>
-      <p className="font-semibold font-montserrat text-clamp-text">{role}</p>
+      <div>
+        <p className="leading-none uppercase font-nats text-clamp-subtitle">{name}</p>
+        <p className="font-semibold font-montserrat text-clamp-text">{role}</p>
+      </div>
     </div>
   );
 }
