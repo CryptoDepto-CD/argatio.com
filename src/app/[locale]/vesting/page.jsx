77ee04396fe07d0eeee744188ce2d58a9faebe8a
dairaespin <dairajazmin.es@gmail.com>
2024-07-04 20:51:16 -0300

    feat(vesting): adding how it works

--- src/app/[locale]/vesting/page.jsx
+++ src/app/[locale]/vesting/page.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/vesting/HeroSection";
+import HowItWorks from "@/components/vesting/HowItWorks";
 
 
 export default function Vesting() {
   return (
     <main className="font-monserrat min-h-screen">
       <HeroSection />
+      <HowItWorks/>
     </main>
   )
 }
